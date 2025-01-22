f7287fd8bc64aff877a4e7ddfa177856f1f70198
2004davidaustin <2004davidaustin@gmail.com>
2025-01-21 19:53:30 -0700

    Rafting website html

---
+++ cse 110/frack.py
@@ -0,0 +1,9 @@
+age = int(input('How old are you??? ')) + 1
+print('''Well golly in one year you'll be ''' + str(age))
+
+eggs = int(input('How many cartons of eggs do you have? ')) * 12
+print('dang you have ' + str(eggs) + ' freakin eggs')
+
+people = int(input('how many strangers are in you walls? '))
+cookies = people / int(input('how many cookies do you have '))
+print('Oh boy you better give ' + str(cookies) + ' cookie to them ' + str(people) + ' or else youll never see the sun for the rest of your life')
