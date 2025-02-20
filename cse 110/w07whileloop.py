34b5431a2bffaec9fd846395a3b38349d836e2e2
2004davidaustin <2004davidaustin@gmail.com>
2025-02-20 15:21:21 -0700

    link fix

---
+++ cse 110/w07whileloop.py
@@ -0,0 +1,9 @@
+number = 0
+answer = "idk maybe"
+
+while number < 1 :
+    number = int(input("Please give me a positive number: "))
+    if number < 1 : print("Bro, literally like what the fetch")
+
+while answer != "yes" :
+    answer = input("Hi I'm small man. Can I have cookie? ").lower()
