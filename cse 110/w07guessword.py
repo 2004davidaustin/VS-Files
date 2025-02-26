ab3c425a4e517bde7dcc0a05b5300173ee3a5358
2004davidaustin <2004davidaustin@gmail.com>
2025-02-25 20:10:41 -0700

    2/25/2025

--- cse 110/w07guessword.py
+++ cse 110/w07guessword.py
@@ -5,29 +5,50 @@ continue_playing = "yes"
 
 while "yes" in continue_playing :
 
+
     print("\nGuess the word!") 
     
+
     #word generator goes here
     word_chooser = ["word", "funny", "cool", "python"]
     word = word_chooser[random.randint(0, 3)]
 
+
     #reset variables
     guess = ""
     guess_count = 0
-    display_word = ""
+    hint_letters = ""
+
 
     while word != guess :
         guess_count += 1
 
 
-        #display_selector = 0
-        #while display_word[display_selector]  #hint machine
+        #hint machine
+        print("HINT:", end=" ")
+        for i, letter in enumerate(word) :
+            if letter in hint_letters :
+                if i == 0 : print(letter.upper(), end=" ")
+                else : print(letter, end=" ")
+            else : print("_", end=" ")
+        print()
         
-        
+
         guess = input("What's your guess? ").lower()
+
+
+        #length verifier
+        while len(word) != len(guess) :
+            print("Guess length must match word length")
+            guess = input("What's your guess? ").lower()
         
+
+        #guess checker
         if guess == word :
             print("YOU GOT IT!!!\n")
             print(f"You guessed: {guess_count} times")
             continue_playing = input("Wanna play again? (Yes or No) ").lower()
-        else : print("Nope.")
+        else :
+            print("Nope.\n")
+            for i, letter in enumerate(word) : #add matching letters to hint_letters
+                if letter in guess and letter not in hint_letters : hint_letters += letter
