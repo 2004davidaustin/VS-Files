cf38dc5a576662bdc4df2ff9a4064e6ae7427d5e
2004davidaustin <2004davidaustin@gmail.com>
2025-03-19 22:01:31 -0600

    3/19/2025

---
+++ cse 110/w11group.py
@@ -0,0 +1,8 @@
+#name id_number job_title salary
+
+with open("hr_system.txt") as hr_system : 
+    for person in hr_system :
+        person = person.strip()
+        person = person.split(" ")
+        
+        print(f"Name: {person[0]}, Title: {person[2]}")
